c3bbb5065a73cdb27a5c02111f20520a5eff7bbc
Akhil <akhilgarg420@gmail.com>
2020-07-27 18:19:45 +0530

    Task 3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import UserOutput from './UserOutput/UserOutput';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Assignment 1</h1>
+      <UserOutput/>
+      <UserOutput/>
     </div>
   );
 }
--- src/UserOutput/UserOutput.js
+++ src/UserOutput/UserOutput.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 const useroutput = () => {
-    <div>
-        <p></p>
-        <p></p>
+    return(
+    <div className='Output'>
+        <p>What's my name? It's Akhil.</p>
+        <p>Oh age, it's 21 or to be precise 21.5 lol</p>
     </div>
+    ) 
 }
 
 export default useroutput;
