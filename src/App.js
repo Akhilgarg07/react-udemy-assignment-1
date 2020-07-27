4f578745f137c46deba2b0a07312b7edffe0a5cb
Akhil <akhilgarg420@gmail.com>
2020-07-27 18:35:46 +0530

    Task 4

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
   return (
     <div className="App">
       <h1>Assignment 1</h1>
-      <UserOutput/>
-      <UserOutput/>
+      <UserOutput username={'akhilgarg07'}/>
+      <UserOutput username={'deadth07'}/>
     </div>
   );
 }
--- src/UserOutput/UserOutput.js
+++ src/UserOutput/UserOutput.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const useroutput = () => {
+const useroutput = (props) => {
     return(
     <div className='Output'>
-        <p>What's my name? It's Akhil.</p>
+        <p>What's my name? It's Akhil or people know me {props.username}</p>
         <p>Oh age, it's 21 or to be precise 21.5 lol</p>
     </div>
     ) 
