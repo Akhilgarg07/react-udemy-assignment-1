5198a20a9835ec652a202db0f66309fb8a709403
Akhil <akhilgarg420@gmail.com>
2020-07-27 20:43:46 +0530

    task 10 - css updated for text-field

--- src/UserInput/UserInput.js
+++ src/UserInput/UserInput.js
@@ -2,9 +2,15 @@ import React from 'react';
 // import './userinput.css'
 
 const userinput = (props) => {
+    const style = {
+    backgroundColor: 'pink',
+      font: 'inherit',
+      border: '5px solid black',
+      padding: '20px auto'
+    }
     return(
     <div className="input">
-        <input type="text" onChange={props.click} value={props.username}/>
+        <input style={style} type="text" onChange={props.click} value={props.username}/>
     </div>
 )};
 
