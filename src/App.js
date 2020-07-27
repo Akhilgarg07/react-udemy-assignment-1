9d9d2970c1836337a8d211d6a026459855d29f90
Akhil <akhilgarg420@gmail.com>
2020-07-27 18:49:10 +0530

    Task 5

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import UserOutput from './UserOutput/UserOutput';
 
-function App() {
+class App extends Component {
+  state = {
+    useroutput:[
+      {username:'akhilgarg07',age:21},
+      {username:'deadth07',age:20}
+    ]
+  }
+  render(){
   return (
     <div className="App">
       <h1>Assignment 1</h1>
-      <UserOutput username={'akhilgarg07'}/>
-      <UserOutput username={'deadth07'}/>
+      <UserOutput username={this.state.useroutput[0].username} age={this.state.useroutput[0].age}/>
+      <UserOutput username={this.state.useroutput[1].username} age={this.state.useroutput[1].age}/>
     </div>
   );
+}
 }
 
 export default App;
--- src/UserOutput/UserOutput.js
+++ src/UserOutput/UserOutput.js
@@ -3,8 +3,8 @@ import React from 'react';
 const useroutput = (props) => {
     return(
     <div className='Output'>
-        <p>What's my name? It's Akhil or people know me {props.username}</p>
-        <p>Oh age, it's 21 or to be precise 21.5 lol</p>
+        <p>What's my name? It's {props.username}</p>
+        <p>Oh age, it's {props.age}</p>
     </div>
     ) 
 }
